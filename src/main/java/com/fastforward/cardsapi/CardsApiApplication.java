3b80c0640fd2ffc0f19eca2672f607f7f0dd7077
Washington <washington.osouza@amedigital.com>
2022-10-08 11:29:44 -0300

    add documentacao do projeto

--- src/main/java/com/fastforward/cardsapi/CardsApiApplication.java
+++ src/main/java/com/fastforward/cardsapi/CardsApiApplication.java
@@ -2,8 +2,10 @@ package com.fastforward.cardsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class CardsApiApplication {
 
 	public static void main(String[] args) {
